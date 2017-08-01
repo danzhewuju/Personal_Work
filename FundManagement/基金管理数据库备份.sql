eed71f29250c72a27ada2f6fd5ae99924709a4e0
余豪 <1668926294@qq.com>
2017-08-02 04:55:56 +0800

    基金笔记本

--- FundManagement/基金管理数据库备份.sql
+++ FundManagement/基金管理数据库备份.sql
@@ -27,12 +27,15 @@ CREATE TABLE IF NOT EXISTS `fund` (
   `manager` varchar(50) NOT NULL DEFAULT '0' COMMENT '基金经理',
   `mgrade` int(11) NOT NULL DEFAULT '0' COMMENT '经理评分',
   PRIMARY KEY (`fid`)
-) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8 COMMENT='基金表';
+) ENGINE=InnoDB AUTO_INCREMENT=100008 DEFAULT CHARSET=utf8 COMMENT='基金表';
 
--- 正在导出表  fund.fund 的数据：~1 rows (大约)
+-- 正在导出表  fund.fund 的数据：~4 rows (大约)
 /*!40000 ALTER TABLE `fund` DISABLE KEYS */;
 REPLACE INTO `fund` (`fid`, `name`, `platform`, `ballpark`, `grade`, `bgrade`, `manager`, `mgrade`) VALUES
-	(100000, 'yuhao', '天使基金', '大', 5, 4, '余豪', 5);
+	(100000, '网易基金', '网易基金', '大', 5, 4, '张三', 5),
+	(100001, '人寿基金', '人寿基金', '中', 4, 3, '张建', 4),
+	(100006, '天使基金', '天使基金平台', '大', 5, 4, '余豪', 5),
+	(100007, '阿里巴巴', '阿里巴巴基金平台', '大', 5, 4, '马云', 5);
 /*!40000 ALTER TABLE `fund` ENABLE KEYS */;
 
 -- 导出  表 fund.invest 结构
@@ -51,19 +54,28 @@ CREATE TABLE IF NOT EXISTS `invest` (
   `rifa` float NOT NULL COMMENT '涨幅',
   `invest` float NOT NULL DEFAULT '5000' COMMENT '投资份额',
   `firstyearprofit` float NOT NULL COMMENT '首日年化',
-  `bsale` int(11) NOT NULL DEFAULT '0' COMMENT '1表示正在投资该基金',
+  `bsale` int(11) NOT NULL DEFAULT '1' COMMENT '1表示正在投资该基金',
   PRIMARY KEY (`iid`),
   KEY `uikey` (`uid`),
   KEY `FK__fund` (`fid`),
   KEY `Index 4` (`iid`),
   CONSTRAINT `FK__fund` FOREIGN KEY (`fid`) REFERENCES `fund` (`fid`),
   CONSTRAINT `uikey` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='信息记录表';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='信息记录表';
 
--- 正在导出表  fund.invest 的数据：~1 rows (大约)
+-- 正在导出表  fund.invest 的数据：~10 rows (大约)
 /*!40000 ALTER TABLE `invest` DISABLE KEYS */;
 REPLACE INTO `invest` (`iid`, `uid`, `fid`, `mode`, `firstdate`, `today`, `getincome`, `thisincome`, `count`, `amount`, `proalo`, `rifa`, `invest`, `firstyearprofit`, `bsale`) VALUES
-	(1, 10000, 100000, '日投', '3917-08-21', '3917-08-21', 12, 25, 1, 12569, 145, 25, 3654, 0.26, 0);
+	(1, 10000, 100000, '日投', '2017-07-21', '2017-07-23', 12, 25, 1, 12569, 145, 25, 3654, 0.05, 1),
+	(2, 10000, 100000, '日投', '2017-07-28', '2017-07-29', 12, 25, 1, 65, 458, 236, 5000, 0.26, 1),
+	(3, 10000, 100001, '周投', '2017-07-29', '2017-07-30', 12, 25, 1, 35, 45, 212, 5000, 0.36, 1),
+	(4, 10001, 100000, '日投', '2017-07-26', '2017-07-30', 12, 25, 1, 35, 45, 212, 5000, 0.36, 1),
+	(8, 10000, 100000, '日投', '3917-08-21', '2017-07-31', 2000, 475, 2, 30000, 2500, 0.0025, 5000, 5.8721, 1),
+	(9, 10000, 100001, '周投', '2017-07-29', '2017-07-31', 2500, 475, 2, 300000, 3000, -0.0012, 1000, 0.5788, 1),
+	(11, 10000, 100006, '日投', '2017-07-31', '2017-07-31', 0, 0, 1, 1000, 0, 0, 1000, 0, 1),
+	(12, 10000, 100000, '日投', '2017-08-02', '2017-08-02', 0, 0, 1, 1000, 0, 0, 1000, 0, 1),
+	(13, 10000, 100006, '日投', '2017-07-31', '2017-08-02', 2000, 950, 2, 360000, 3000, 0.00001, 5000, 0.9657, 1),
+	(14, 10000, 100000, '日投', '2017-08-21', '2017-08-03', 2000, 950, 2, 360000, 3000, 0.00001, 500, 0.9657, 1);
 /*!40000 ALTER TABLE `invest` ENABLE KEYS */;
 
 -- 导出  表 fund.user 结构
@@ -77,8 +89,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- 正在导出表  fund.user 的数据：~2 rows (大约)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 REPLACE INTO `user` (`uid`, `password`, `account`) VALUES
-	(10000, '1', 2),
-	(10001, '2', 3);
+	(10000, 'root', 0),
+	(10001, 'root', 0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
